0fef8aba9a91ee56922b6d99dad842a35dce1dcd
Yuya.Nishida <yuya@j96.org>
2014-02-03 21:54:55 +0900

    support setup database.

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,12 +10,21 @@
 include_recipe "ruby_build"
 include_recipe "rbenv::system"
 include_recipe "passenger_apache2"
+include_recipe "postgresql::server"
+include_recipe "database"
+include_recipe "database::postgresql"
 
 root = node["rails_application_server"]
 base_path = Pathname(root["base_path"])
 root["applications"].each do |name, c|
   home_path = base_path + name
   user_name = name
+  database_user_name = name
+  database_server_connection_configuration = {
+    host: "localhost",
+    username: "postgres",
+    password: node["postgresql"]["password"]["postgres"],
+  }
 
   user user_name do
     comment "an user for #{name}"
@@ -41,6 +50,49 @@ root["applications"].each do |name, c|
   rbenv_gem "bundler" do
     rbenv_version c["ruby_version"]
     action :install
+  end
+
+  application_shared_path = home_path + "shared"
+  directory application_shared_path.to_s do
+    owner user_name
+    mode 0755
+  end
+
+  application_config_path = application_shared_path + "config"
+  directory application_config_path.to_s do
+    owner user_name
+    mode 0755
+  end
+
+  template "database.yml" do
+    path (application_config_path + "database.yml").to_s
+    owner user_name
+    mode 0600
+    source "database.yml.erb"
+    variables({
+                name: name,
+                database_user: database_user_name,
+                database_password: c["database_password"],
+              })
+  end
+
+  %w(production test).each do |environment|
+    d = "#{name}_#{environment}"
+
+    database_user database_user_name do
+      provider Chef::Provider::Database::PostgresqlUser
+      connection database_server_connection_configuration
+      password c["database_password"]
+      database_name d
+      privileges [:all]
+      action :create
+    end
+
+    database d do
+      provider Chef::Provider::Database::Postgresql
+      connection database_server_connection_configuration
+      action :create
+    end
   end
 
   web_app name do
